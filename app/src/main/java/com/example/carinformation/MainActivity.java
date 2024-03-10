065fbd0cf92842c5a72a627b4019d16b83bc2290
danish-repos <khurraimmughal@gmail.com>
2024-03-10 23:38:43 +0500

    Initial commit

---
+++ app/src/main/java/com/example/carinformation/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.carinformation;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+
+    }
+
+    private void init () {
+
+    }
+
+
+}
